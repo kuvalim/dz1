2c8b49ce42389204e9e1383617361d1d12e7bb14
kuvalim <kuvalim@gmail.com>
2016-11-30 09:23:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            char[] ch = str.ToCharArray();
+            var count = ch.Where((n) => n >= '0' && n <= '9').Count();
+            Console.WriteLine("Количество цифр в строке: " + count);
+            Console.ReadKey();
+        }
+    }
+}
